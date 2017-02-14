8f554474da29d8ece29e8fa5e1d713143726f18c
Caio Lopes <caiodesalopes@gmail.com>
2017-02-14 00:19:27 -0200

    Add automatic tests

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, Route, IndexRoute, hashHistory} from 'react-router';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import Main from 'Main';
 import Timer from 'Timer';
 import Countdown from 'Countdown';
