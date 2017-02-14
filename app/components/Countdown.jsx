ff0f1999da2b1456f5377b1393745525b851c7c6
Caio Lopes <caiodesalopes@gmail.com>
2017-02-13 23:29:17 -0200

    Initial commit

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Countdown = (props) => {
+  return (
+    <div>Countdown</div>
+  );
+};
+
+export default Countdown;
---
+++ app/components/Timer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Timer = (props) => {
+  return (
+    <div>Timer</div>
+  );
+};
+
+export default Timer;
