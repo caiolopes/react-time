3ae250797f65d98f54a8b3dec47c45f8612d20e0
Caio Lopes <caiodesalopes@gmail.com>
2017-02-19 23:29:46 -0300

    Add coutdown functionality and tests

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -3,20 +3,42 @@ import Clock from 'Clock';
 import CountdownForm from 'CountdownForm';
 
 export default class Countdown extends Component {
+  // old style
   // constructor(props) {
   //   super(props);
   //   this.handleSetCountdown = this.handleSetCountdown.bind(this);
   //   this.state = { count: 0 };
   // }
-  // alternative:
 
   state = {
-    count: 0
+    count: 0,
+    countdownStatus: 'stopped'
   };
+  
+  // it is called everytime props or state updates
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.countdownStatus !== prevState.countdownStatus) {
+      switch(this.state.countdownStatus) {
+        case 'started':
+          this.startTimer();
+          break;
+      }
+    }
+  }
+
+  startTimer = () => {
+    this.timer = setInterval(() => {
+      const newCount = this.state.count - 1;
+      this.setState({
+        count: newCount >= 0 ? newCount : 0
+      });
+    }, 1000);
+  }
   
   handleSetCountdown = (seconds) => {
     this.setState({
-      count: seconds
+      count: seconds,
+      countdownStatus: 'started'
     });
   }
   
