edadd637a2af286cffc3b0973d7f327ad537cd3c
Caio Lopes <caiodesalopes@gmail.com>
2017-03-09 23:43:41 -0300

    Correct typo

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -6,7 +6,7 @@ const Main = (props) => {
     <div>
       <Navigation />
       <div className="row">
-        <div className="column small-centered medium-6 large-4">
+        <div className="columns small-centered medium-6 large-4">
           {props.children}
         </div>
       </div>
